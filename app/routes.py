6eb8c6af628ed85a322c14b65fbb591de8f4fb92
thekakodkar <niraj.kakodkar@gmail.com>
2020-06-23 23:42:26 +0300

    Added Template

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hey guys wassup"
+        user = {'username': 'Niraj'}
+        return '''
+    <html>
+        <head>
+            <title>ServiceMan</title>
+        </head>
+        <body>
+            <h1>Moi, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
