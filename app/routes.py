14765339d73757c197128059c15b114c3426e480
thekakodkar <niraj.kakodkar@gmail.com>
2020-06-24 00:09:09 +0300

    Improved Templating by using base template

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-        user = {'username': 'Niraj'}
-        return '''
-    <html>
-        <head>
-            <title>ServiceMan</title>
-        </head>
-        <body>
-            <h1>Moi, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>'''
+    user = {'username': 'Niraj'}
+    posts = [
+            {
+                    'author': {'username': 'John'},
+                    'body': 'Beautiful day in Portland!'
+            },
+            {
+                    'author': {'username': 'Susan'},
+                    'body': 'The Avengers movie was so cool!'
+            }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
