6a50757639a227093ca7fbf7fd08a39023389714
Niraj Kakodkar <niraj.kakodkar@gmail.com>
2020-07-09 01:50:26 +0300

    Used 'url_for' function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
@@ -18,7 +19,13 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+from flask import render_template, flash, redirect
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(( url_for('index') ))
     return render_template('login.html', title='Sign In', form=form)
